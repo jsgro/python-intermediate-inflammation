2d145b63ed5b8849d892fd8195c2d31f6cec98f8
Jean-Yves Sgro <jsgro@wisc.edu>
2023-03-15 11:20:07 -0500

    add dev-std function

--- inflammation/models.py
+++ inflammation/models.py
@@ -20,8 +20,10 @@ def load_csv(filename):
 
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array.
-       :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
-       :returns: An array of mean values of measurements for each day.
+
+    :param data: A 2D data array with inflammation data 
+    (each row contains measurements for a single patient across all days).
+    :returns: An array of mean values of measurements for each day.
     """
     return np.mean(data, axis=0)
 
@@ -29,7 +31,8 @@ def daily_mean(data):
 def daily_max(data):
     """Calculate the daily max of a 2D inflammation data array.
 
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :param data: A 2D data array with inflammation data 
+    (each row contains measurements for a single patient across all days).
     :returns: An array of max values of measurements for each day.
     """
     return np.max(data, axis=0)
@@ -37,8 +40,40 @@ def daily_max(data):
 
 def daily_min(data):
     """Calculate the daily min of a 2d inflammation data array.
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :param data: A 2D data array with inflammation data 
+    (each row contains measurements for a single patient across all days).
     :returns: An array of minimum values of measurements for each day.
     """
     return np.min(data, axis=0)
 
+def patient_normalise(data):
+    """
+    Normalise patient data between 0 and 1 of a 2D inflammation data array.
+
+    Any NaN values are ignored, and normalised to 0
+
+    :param data: 2D array of inflammation data
+    :type data: ndarray
+
+    """
+    if not isinstance(data, np.ndarray):
+        raise TypeError('data input should be ndarray')
+    if len(data.shape) != 2:
+        raise ValueError('inflammation array should be 2-dimensional')
+    if np.any(data < 0):
+        raise ValueError('inflammation values should be non-negative')
+    max_data = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / max_data[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    return normalised
+def patient_std(data):
+    """Compute standard deviation for each patient on original 
+    inflammation data 2D array.
+    
+    :param data: 2D array of inflammation data
+    :type data: ndarray
+    
+    """
+    # To compute for each row we need to use axis=1
+    return np.std(data, axis=1)
